e32eef0e3bf3fc6f4f9df812850cf56bef835a4d
Erica Stewart <114890276+esrtewatl@users.noreply.github.com>
2023-02-27 10:15:55 -0500

    header

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>This is the header</h1>
+      <div className='header-left'>This is the right header</div>
+      <div className='header-right'>This is the left header</div>
+    </div>
+  )
+}
+
+export default Header
+ 
