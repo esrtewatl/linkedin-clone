c96d173f9bba79f0efa9c14126785d860d757379
Erica Stewart <114890276+esrtewatl@users.noreply.github.com>
2023-02-28 03:57:18 -0500

    more header    stuff

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,9 @@ import SearchIcon from '@mui/icons-material/Search';
 import HeaderOption from './HeaderOption';
 import HomeIcon from '@mui/icons-material/Home';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
-
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications'; 
 
 function Header() {
   return (
@@ -27,7 +29,10 @@ function Header() {
 
 <HeaderOption Icon={HomeIcon} title='Home' />
 <HeaderOption Icon={SupervisorAccountIcon}  title='My Network' />
-
+<HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+<HeaderOption Icon={ChatIcon} title='Messaging'/>
+<HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+<HeaderOption avatar='./profile.png'/>
 
       </div>
     </div>
